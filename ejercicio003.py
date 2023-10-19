130f0f5bec3477c054501997e970df1f0058dcfb
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-19 09:58:47 -0300

    546

--- ejercicio003.py
+++ ejercicio003.py
@@ -1,19 +1,20 @@
-enunciado ="""
+enunciado = """
 Realizá un programa que permita que el usuario ingrese su nombre. 
 El programa debe emitir una salida con un mensaje de bienvenida que 
-incluya el nombre ingresado
+incluya el nombre ingresado.
 """
 
-
 print(enunciado)
-nombre_usuario = input("ingrese su nombre: ")
-print("Hola",nombre_usuario)
-cartel = "Hola "+nombre_usuario +" como estas." 
-print(cartel)
 
+while True:
+    nombre_usuario = input("Ingrese su nombre (o escriba 'salir' para finalizar): ")
 
+    if nombre_usuario.lower() == "salir":
+        break
 
-
+    print("Hola", nombre_usuario)
+    cartel = "Hola " + nombre_usuario + ", ¿cómo estás?"
+    print(cartel)
 
 
 
